296cb987ae41627c90c9599590dd7cbf4ef27bc3
Rasmus <rasmus.eriksson@fast2.se>
2021-07-20 11:17:15 +0200

    rolodex with search feature and some api calls

---
+++ monsters-rolodex/src/App.js
@@ -0,0 +1,45 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+import { SearchBox } from './components/search-box/search-box.component';
+import { CardList } from './components/card-list/card-list.component';
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }))
+  }
+
+  handleSearch = (e) => {
+    this.setState({ searchField: e.target.value })
+  }
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonster = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
+    return (
+      <div className="App">
+        <SearchBox
+          placeholder='Search monster'
+          handleChange={this.handleSearch}
+        />
+        <CardList monsters={filteredMonster} />
+      </div>
+    )
+  }
+}
+
+export default App;
